62b3fd423530af995df37d9fbdb3353b2090f128
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-25 14:54:59 +0200

    adding attributes to fields to generate correct ExtentReport

--- TestApiProject/Config/ReportBindings.cs
+++ TestApiProject/Config/ReportBindings.cs
@@ -11,22 +11,16 @@ namespace TestApiProject.Tests.Config
     public class ReportBindings
     {
         private static ExtentReports _extent;
-        private static ExtentTest _featureName;
-        private static ExtentTest _scenario;
+
+        [ThreadStatic] private static ExtentTest _featureName;
+
+        [ThreadStatic] private static ExtentTest _scenario;
+
         private readonly ScenarioContext _scenarioContext;
 
         public ReportBindings(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
-        }
-
-        [BeforeTestRun]
-        private static void BeforeTestRun()
-        {
-            var htmlReporter = new ExtentHtmlReporter(AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug\\net5.0", "\\Report"));
-            htmlReporter.Config.Theme = Theme.Standard;
-            _extent = new ExtentReports();
-            _extent.AttachReporter(htmlReporter);
         }
 
         [BeforeFeature]
@@ -38,7 +32,12 @@ namespace TestApiProject.Tests.Config
         [BeforeScenario(Order = 20)]
         public void BeforeScenario()
         {
-            _scenario = _featureName.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title + $" {_scenarioContext.ScenarioInfo.Arguments[0]}");
+            var possibleArgument = "";
+            if (_scenarioContext.ScenarioInfo.Arguments.Count != 0)
+                possibleArgument = _scenarioContext.ScenarioInfo.Arguments[0]
+                                                   ?.ToString();
+
+            _scenario = _featureName.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title + $" {possibleArgument}");
         }
 
         [AfterStep(Order = 20)]
@@ -66,19 +65,19 @@ namespace TestApiProject.Tests.Config
                 {
                     case "Given":
                         _scenario.CreateNode<Given>(_scenarioContext.StepContext.StepInfo.Text)
-                                .Fail(_scenarioContext.TestError.Message);
+                                 .Fail(_scenarioContext.TestError.Message);
                         break;
                     case "When":
                         _scenario.CreateNode<When>(_scenarioContext.StepContext.StepInfo.Text)
-                                .Fail(_scenarioContext.TestError.Message);
+                                 .Fail(_scenarioContext.TestError.Message);
                         break;
                     case "Then":
                         _scenario.CreateNode<Then>(_scenarioContext.StepContext.StepInfo.Text)
-                                .Fail(_scenarioContext.TestError.Message);
+                                 .Fail(_scenarioContext.TestError.Message);
                         break;
                     case "And":
                         _scenario.CreateNode<And>(_scenarioContext.StepContext.StepInfo.Text)
-                                .Fail(_scenarioContext.TestError.Message);
+                                 .Fail(_scenarioContext.TestError.Message);
                         break;
                 }
         }
@@ -87,6 +86,16 @@ namespace TestApiProject.Tests.Config
         public static void AfterTestRun()
         {
             _extent.Flush();
+        }
+
+        [BeforeTestRun]
+        private static void BeforeTestRun()
+        {
+            var htmlReporter = new ExtentHtmlReporter(AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug\\net5.0", "\\Report"));
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.ReportName = "zippopotamus API test automation report";
+            _extent = new ExtentReports();
+            _extent.AttachReporter(htmlReporter);
         }
     }
 }
