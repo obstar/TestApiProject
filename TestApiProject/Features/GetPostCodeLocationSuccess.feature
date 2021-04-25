f3ea6bd9ca3861715a9acc19c5f08b4fca4bb9f7
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-25 12:14:43 +0200

    added new steps to scenarios in GetPostCodeLocationSuccess.feature

--- TestApiProject/Features/GetPostCodeLocationSuccess.feature
+++ TestApiProject/Features/GetPostCodeLocationSuccess.feature
@@ -1,13 +1,17 @@
-﻿Feature: GetPostCodeLocationSuccess
+﻿@PostCode
+Feature: GetPostCodeLocationSuccess
 	
 
-Scenario: GetPostCodeLocationSuccess - Successful request for different countries
-	Given I perform get request to endpoint for '<countryCode>' country code with '<postCode>' post code
-	When I get address from post code get response
-	Then the post code get request is 'OK'
-		And the post code from response is '90210'
-		And the post code from response is '90210'
-	Examples:
-        | example description   | countryCode | postCode |
-		| Poland address        | PL          | 32-600   |
-		| United States address | US          | 90210    |
+Scenario Outline: GetPostCodeLocationSuccess - Successful request for different countries
+	Given I perform get request to endpoint for <countryCode> country code with <postCode> post code
+	When I have body from get post code location response
+	Then the response status code is 'OK'
+		And the response has post code equal to <postCode>
+		And the response has country equal to <countryName>
+		And the response has country abbreviation equal to <countryCode>
+		And the response has places equal to <placeName>, <state>, <stateCode>, <longitude> and <latitude>
+	Examples: 
+        | example description   | countryName   | countryCode | postCode | placeName     | state        | stateCode | longitude | latitude |
+		| Poland address        | Poland        | PL          | 32-600   | Oświęcim      | Małopolskie  |           | 19.2333   | 50.0333  |
+		| United States address | United States | US          | 90210    | Beverly Hills | California   | CA        | -118.4065 | 34.0901  |
+		| Austria address       | Austria       | AT          | 1010     | Wien          |              |           | 16.3705   | 48.2077  |
--- TestApiProject/ObjectsSteps/GetPostCodeLocationSteps.cs
+++ TestApiProject/ObjectsSteps/GetPostCodeLocationSteps.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -6,6 +7,7 @@ using RestSharp;
 using TechTalk.SpecFlow;
 using TestApiProject.Tests.Api.Calls;
 using TestApiProject.Tests.Api.Models;
+using TestApiProject.Tests.Config;
 
 namespace TestApiProject.Tests.ObjectsSteps
 {
@@ -13,12 +15,14 @@ namespace TestApiProject.Tests.ObjectsSteps
     [Scope(Tag = "@PostCode")]
     public class GetPostCodeLocationSteps
     {
+        private readonly AssertExtension _assertExtension;
         private readonly GetPostCodeLocationApiCall _getPostCodeLocationApiCall;
         private IRestResponse _fullResponse;
         private GetPostCodeLocationModel _postCodeLocationBody;
 
-        public GetPostCodeLocationSteps(GetPostCodeLocationApiCall getPostCodeLocationApiCall)
+        public GetPostCodeLocationSteps(AssertExtension assertExtension,GetPostCodeLocationApiCall getPostCodeLocationApiCall)
         {
+            _assertExtension = assertExtension;
             _getPostCodeLocationApiCall = getPostCodeLocationApiCall;
         }
 
@@ -27,7 +31,7 @@ namespace TestApiProject.Tests.ObjectsSteps
         public async Task GivenIPerformGetRequestToEndpointForCountryCodeWithPostCode(string countryCode, 
                                                                                       string postCode)
         {
-            _fullResponse = await _getPostCodeLocationApiCall.Get(countryCode, postCode);
+            _fullResponse = await _getPostCodeLocationApiCall.GetAsync(countryCode, postCode);
         }
 
         [When(@"I have body from get post code location response")]
@@ -36,10 +40,42 @@ namespace TestApiProject.Tests.ObjectsSteps
             _postCodeLocationBody = JsonConvert.DeserializeObject<GetPostCodeLocationModel>(_fullResponse.Content);
         }
 
-        [Then(@"the request status code is '(.*)'")]
-        public void ThenTheRequestStatusCodeIs(string httpStatusCode)
+        [Then(@"the response status code is '(.*)'")]
+        public void ThenTheResponseStatusCodeIs(string httpStatusCode)
         {
-            Assert.AreEqual(httpStatusCode, _fullResponse.StatusCode.ToString());
+            _assertExtension.AreEqual(httpStatusCode, _fullResponse.StatusCode.ToString());
+        }
+
+        [Then(@"the response has post code equal to (.*)")]
+        public void ThenTheResponseHasPostCodeEqualTo(string postCode)
+        {
+            _assertExtension.AreEqual(postCode, _postCodeLocationBody.PostCode);
+        } 
+        
+        [Then(@"the response has country equal to (.*)")]
+        public void ThenTheResponseHasCountryEqualTo(string countryName)
+        {
+            _assertExtension.AreEqual(countryName, _postCodeLocationBody.Country);
+        }
+
+        [Then(@"the response has country abbreviation equal to (.*)")]
+        public void ThenTheResponseHasCountryAbbreviationEqualTo(string countryCode)
+        {
+            _assertExtension.AreEqual(countryCode, _postCodeLocationBody.CountryAbbreviation);
+        }
+
+        [Then(@"the response has places equal to (.*), (.*), (.*), (.*) and (.*)")]
+        public void ThenTheResponseHasPlacesEqualToAnd(string placeName, 
+                                                       string state, 
+                                                       string stateCode, 
+                                                       string longitude, 
+                                                       string latitude)
+        {
+            _assertExtension.AreEqual(placeName, _postCodeLocationBody.Places[0].PlaceName);
+            _assertExtension.AreEqual(state, _postCodeLocationBody.Places[0].State);
+            _assertExtension.AreEqual(stateCode, _postCodeLocationBody.Places[0].StateAbbreviation);
+            _assertExtension.AreEqual(longitude, _postCodeLocationBody.Places[0].Longitude);
+            _assertExtension.AreEqual(latitude, _postCodeLocationBody.Places[0].Latitude);
         }
     }
 }
