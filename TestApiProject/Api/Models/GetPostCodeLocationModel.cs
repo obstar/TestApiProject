93d3e7ec1dce0c16e042d99070f5aee63ba5567c
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-25 09:58:45 +0200

    added new class GetPostCodeLocationModel

---
+++ TestApiProject/Api/Models/GetPostCodeLocationModel.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace TestApiProject.Tests.Api.Models
+{
+    public class GetPostCodeLocationModel
+    {
+        [JsonProperty("post code")]
+        public string PostCode { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("country abbreviation")]
+        public string CountryAbbreviation { get; set; }
+
+        [JsonProperty("places")]
+        public Place[] Places { get; set; }
+    }
+
+    public class Place
+    {
+        [JsonProperty("place name")]
+        public string PlaceName { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("state abbreviation")]
+        public string StateAbbreviation { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+    }
+}
