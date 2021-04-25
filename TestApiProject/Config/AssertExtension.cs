2309bb1b94d7233ab4ac810de2945ac6bcf7775a
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-25 12:13:13 +0200

    added new class AssertExtension.cs

---
+++ TestApiProject/Config/AssertExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApiProject.Tests.Config
+{
+    public class AssertExtension
+    {
+        public void AreEqual(Object expected, Object actual)
+        {
+            Console.WriteLine($"\t\t-> Expected: {expected}");
+            Console.WriteLine($"\t\t-> Actual: {actual}");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
